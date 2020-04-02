3ee1f29b1a368ce96f69db3e3cc38b8a25929c95
Nitika <nitika@wetransfer.com>
2020-04-02 22:58:51 +0200

    Send mail notification for product expiry to all the users. Next step is to add a scheduler on top of this so that the job runs daily.

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,10 +1,15 @@
 require_relative 'application_mailer'
 class NotificationMailer < ApplicationMailer
-  default from:'nitika@wetransfer.com'
-  layout  'mailer'
 
-  def notification_email(email)
-    mail(to: email, subject: 'Important notification regarding expired products,
-      you might be using right now..!!')
+  def notification_email
+    @message = ''
+    User.all.each do |user|
+      Product.where('expiry_date > current_date - interval \'5\' day').each do |product|
+        @message = @message + product.name + ':' + "\t" + product.expiry_date.to_s + "\n"
+      end
+      @name = user.name
+      @email = user.email
+      mail(to: @email, subject: 'Items expiring soon..!!')
+    end
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,23 +1,24 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
 # require "sprockets/railtie"
-require "rails/test_unit/railtie"
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv.load
 
 module Productexpirymanager
   class Application < Rails::Application
@@ -33,5 +34,16 @@ module Productexpirymanager
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.smtp_settings = {
+        address: ENV['SMTP_HOST'],
+        port: ENV['SMTP_PORT'],
+        domain: ENV['DOMAIN'],
+        user_name: ENV['SMTP_USER'],
+        password: ENV['SMTP_PASSWORD'],
+        authentication: :plain,
+        enable_starttls_auto: true
+    }.compact
   end
 end
