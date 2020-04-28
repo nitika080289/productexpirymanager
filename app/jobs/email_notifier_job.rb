ea2933ddf6c30d4656754b15a70bf43fec07d92d
Nitika <nitika@wetransfer.com>
2020-04-28 17:19:24 +0200

    Refactored sidekiq job code based on some earlier review comments as multiple emails were not getting delivered on time

--- app/jobs/email_notifier_job.rb
+++ app/jobs/email_notifier_job.rb
@@ -1,6 +1,15 @@
 class EmailNotifierJob < ApplicationJob
   queue_as :default
   def perform(*args)
-    NotificationMailer.notification_email.deliver!
+    message = ''
+    User.all.each do |user|
+      Product.where('expiry_date - interval \'5\' day < current_date and
+        user_id = :id', id: user.id).each do |product|
+        message = message + product.name + ':' + "\t" +
+            product.expiry_date.to_s + "\n"
+      end
+      NotificationMailer.notification_email(user.name, user.email, message).deliver
+      message = ''
+      end
   end
 end
