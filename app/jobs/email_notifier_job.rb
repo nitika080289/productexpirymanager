595f3d678a4da23de25aa7472ef4d7a24727541d
Nitika <nitika@wetransfer.com>
2020-04-28 17:28:36 +0200

    Code review changes

--- app/jobs/email_notifier_job.rb
+++ app/jobs/email_notifier_job.rb
@@ -2,11 +2,11 @@ class EmailNotifierJob < ApplicationJob
   queue_as :default
   def perform(*args)
     message = ''
-    User.all.each do |user|
-      Product.where('expiry_date - interval \'5\' day < current_date and
-        user_id = :id', id: user.id).each do |product|
+    User.find_each do |user|
+      Product.where('expiry_date < :date and user_id = :id',
+                    id: user.id, date: 5.days.from_now).each do |product|
         message = message + product.name + ':' + "\t" +
-            product.expiry_date.to_s + "\n"
+                  product.expiry_date.to_s + "\n"
       end
       NotificationMailer.notification_email(user.name, user.email, message).deliver
       message = ''
