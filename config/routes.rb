e2f1832f01191e3ecc6f1615a044055822ac50b7
Nitika <nitika@wetransfer.com>
2020-04-28 22:45:49 +0200

    Merge changes with master

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
   require 'sidekiq/cron/web'
   mount Sidekiq::Web => '/sidekiq'
-Rails.application.routes.draw do  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users do
     resources :products
   end
