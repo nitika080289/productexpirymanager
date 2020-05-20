cbf5b63724fe5b3739ca091dc1c28acd61f57727
Nitika <nitika@wetransfer.com>
2020-05-20 16:27:01 +0200

    Updated change from ui-with-react branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,8 @@ class User < ApplicationRecord
 
   def self.create_from_omniauth(auth, cookies)
     # Creates a new user only if it doesn't exist
-    User.find_or_create_by(email: auth.info.email) do |user|
-      user.name = auth.info.name
-      # create cookie after user is made
-      cookies.encrypted[:current_user_id] = { value: user.id, expires: 7.days.from_now }
-    end
+    user = User.create_with(name: auth.info.name).find_or_create_by(email: auth.info.email)
+    # create cookie after user is made
+    cookies.encrypted[:current_user_id] = { value: user.id, expires: 7.days.from_now }
   end
 end
